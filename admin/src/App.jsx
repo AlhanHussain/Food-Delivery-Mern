8188e361d858d9611dc8eb05b17bce3a8038f1b2
Alhan Hussain <143873845+AlhanHussain@users.noreply.github.com>
2024-09-07 09:59:25 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-mern-l5kk.onrender.com"
 
   return (
     <div>
